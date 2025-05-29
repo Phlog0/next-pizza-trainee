63426de567f8cd59310e53f0bdd3f14ef03fcdf5
phlog0 <sergeyzadorkin.w@gmail.com>
2025-05-29 21:03:08 +0300

    last-commit-before-linux

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
 import {
   Container,
   Filters,
-  ProductCard,
   Title,
   TopBar,
 } from "@/components/shared";
+import { ProductsGroupList } from "@/components/shared/products-group-list";
 
-import Image from "next/image";
 
 export default function Home() {
   return (
@@ -16,16 +15,54 @@ export default function Home() {
       </Container>
       <TopBar />
       <Container className="pb-14 mt-9">
-        <div className="flex gap-[60px]">
-          <div className="w-[250px">
+        <div className="flex gap-[80px]">
+          <div className="shrink-0">
             <Filters />
           </div>
           <div className="flex flex-col gap-16">
-            <ProductCard
-              id={1}
-              title="pizza"
-              imageUrl="https://media.dodostatic.net/image/r:584x584/0194491914e478b4aa3e18d44e07eed9.avif"
-              price={300}
+            <ProductsGroupList
+              title="Пицца"
+              categoryId={1}
+              items={[
+                {
+                  id: 1,
+                  title: "Чизбургер-пицца",
+                  imageUrl:
+                    "https://media.dodostatic.net/image/r:584x584/11ee7d612fc7b7fca5be822752bee1e5.avif",
+                  price: 550,
+                  items: [{ price: 550 }],
+                },
+                {
+                  id: 2,
+                  title: "Чизбургер-пицца",
+                  imageUrl:
+                    "https://media.dodostatic.net/image/r:584x584/11ee7d612fc7b7fca5be822752bee1e5.avif",
+                  price: 550,
+                  items: [{ price: 550 }],
+                },
+              ]}
+            />
+            <ProductsGroupList
+              title="Комбо"
+              categoryId={2}
+              items={[
+                {
+                  id: 1,
+                  title: "Чизбургер-пицца",
+                  imageUrl:
+                    "https://media.dodostatic.net/image/r:584x584/11ee7d612fc7b7fca5be822752bee1e5.avif",
+                  price: 550,
+                  items: [{ price: 550 }],
+                },
+                {
+                  id: 2,
+                  title: "Чизбургер-пицца",
+                  imageUrl:
+                    "https://media.dodostatic.net/image/r:584x584/11ee7d612fc7b7fca5be822752bee1e5.avif",
+                  price: 550,
+                  items: [{ price: 550 }],
+                },
+              ]}
             />
           </div>
         </div>
--- components/shared/categories.tsx
+++ components/shared/categories.tsx
@@ -1,28 +1,33 @@
+"use client";
 import { cn } from "@/lib/utils";
+import { useCategoryStore } from "@/store/category";
+import { Button } from "../ui";
 
 const cats = [
-  "Пицца",
-  "Комбо",
-  "Закуски",
-  "Коктейли",
-  "Кофе",
-  "Напитки",
-  "Десерты",
+  { id: 1, name: "Пицца" },
+  { id: 2, name: "Комбо" },
+  { id: 3, name: "Закуски" },
+  { id: 4, name: "Коктейли" },
+  { id: 5, name: "Кофе" },
+  { id: 6, name: "Напитки" },
+  { id: 7, name: "Десерты" },
 ];
-const activeIndex = 0;
+
 export function Categories({ className }: { className?: string }) {
+  const categoryActiveId = useCategoryStore((state) => state.activeId);
   return (
     <div className={cn("inline-flex gap-3 bg-gray-50 rounded-2xl", className)}>
-      {cats.map((cat, index) => (
+      {cats.map(({ name, id }, index) => (
         <a
           key={index}
           className={cn(
-            "flex items-center justify-center font-bold px-5 h-11 rounded-2xl",
-            activeIndex === index &&
-              "bg-white shadow-md shadow-gray-200 text-primary"
+            "flex items-center justify-center font-bold px-5 h-11 rounded-2xl cursor-pointer",
+            categoryActiveId === id &&
+              "bg-red-300 shadow-md shadow-gray-200 text-primary"
           )}
+          href={`/#${name}`}
         >
-          <button>{cat}</button>
+          <Button>{name}</Button>
         </a>
       ))}
     </div>
